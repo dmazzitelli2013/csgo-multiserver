820520a4becba3049b619b5c092baaa7e645575b
David Mazzitelli <davidma1984@gmail.com>
2019-03-24 04:32:02 -0300

    Updates for 2vs2 matches

---
+++ apps/csgo/scripts/modes/competitive2vs2.conf.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# (C) 2016 Maximilian Wende <maximilian.wende@gmail.com>
+#
+# This file is licensed under the Apache License 2.0. For more information,
+# see the LICENSE file or visit: http://www.apache.org/licenses/LICENSE-2.0
+
+MAXPLAYERS="4"
+
+MAPGROUP=${MAPGROUP-"mg_active"}
+MAPS=${MAPS-"de_shortdust"}
+
